1a16729e9b63a686b9d4575e59395bacf87e7e86
Ilnur Nasyrov <ilnur.nasyrov.92@gmail.com>
2017-12-29 03:55:24 +0300

    Add native implementation

--- lib/inject_context.rb
+++ lib/inject_context.rb
@@ -1,5 +1,39 @@
 require "inject_context/version"
 
 module InjectContext
-  # Your code goes here...
+  def self.[](*dependencies)
+    new_module = InjectContext::Injection.dup
+    new_module.define_helpers(*dependencies)
+    new_module
+  end
 end
+
+module InjectContext::Injection
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def self.define_helpers(*dependencies, **renamed_dependencies)
+    dependencies.each do |dependency|
+      define_method(dependency) do
+        @_context[dependency]
+      end
+    end
+
+    renamed_dependencies.each do |old_name, new_name|
+      define_method(new_name) do
+        @_context[old_name]
+      end
+    end
+  end
+
+  module ClassMethods
+    def build(context = nil, *options)
+      instance = new(*options)
+      instance.instance_variable_set('@_context', context)
+      instance
+    end
+  end
+end
+
+InjectContext::Injection.freeze
--- spec/inject_context_spec.rb
+++ spec/inject_context_spec.rb
@@ -1,9 +1,63 @@
 RSpec.describe InjectContext do
-  it "has a version number" do
-    expect(InjectContext::VERSION).not_to be nil
+  it { expect(InjectContext::VERSION).not_to be nil }
+
+  it "adds .build method" do
+    klass = Class.new { include InjectContext[] }
+
+    expect(klass.build).to be_instance_of klass
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "takes first argument as context and saves to @_context" do
+    klass = Class.new { include InjectContext[] }
+
+    context = Container.new
+    instance = klass.build(context)
+
+    expect(instance.instance_variable_get("@_context")).to eq context
+  end
+
+  it "passes arguments from build to new" do
+    klass =
+      Class.new do
+        include InjectContext[]
+
+        attr_reader :args
+
+        def initialize(*args)
+          @args = args
+        end
+      end
+
+    expect(klass.build(nil, 1, 2).args).to eq [1, 2]
+  end
+
+  it "defines getters for context" do
+    klass = Class.new { include InjectContext[:repo] }
+
+    context = Container.new(repo: double(:repo))
+    instance = klass.build(context)
+
+    expect(instance).to respond_to :repo
+    expect(instance.repo).to eq context[:repo]
+  end
+
+  it "allows use alternative names for getters" do
+    klass = Class.new { include InjectContext[repo: :some_repo] }
+
+    context = Container.new(repo: double(:repo))
+    instance = klass.build(context)
+
+    expect(instance).to respond_to :some_repo
+    expect(instance.some_repo).to eq context[:repo]
+  end
+end
+
+class Container
+  def initialize(**dependencies)
+    @dependencies = dependencies
+  end
+
+  def [](name)
+    @dependencies.fetch(name)
   end
 end
