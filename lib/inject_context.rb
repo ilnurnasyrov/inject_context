7be6b7dc9bfd73509b44edb270fc018c213b9241
Ilnur Nasyrov <ilnur.nasyrov.92@gmail.com>
2017-12-29 04:08:29 +0300

    Clarify lib/inject_context.rb

--- lib/inject_context.rb
+++ lib/inject_context.rb
@@ -1,39 +1,37 @@
 require "inject_context/version"
 
 module InjectContext
-  def self.[](*dependencies)
-    new_module = InjectContext::Injection.dup
-    new_module.define_helpers(*dependencies)
-    new_module
+  def self.[](*args)
+    injection = InjectContext::Injection.dup
+    injection.define_helpers(*args)
+    injection
   end
 end
 
 module InjectContext::Injection
   def self.included(base)
-    base.extend(ClassMethods)
-  end
-
-  def self.define_helpers(*dependencies, **renamed_dependencies)
-    dependencies.each do |dependency|
-      define_method(dependency) do
-        @_context[dependency]
-      end
-    end
-
-    renamed_dependencies.each do |old_name, new_name|
-      define_method(new_name) do
-        @_context[old_name]
-      end
-    end
-  end
-
-  module ClassMethods
-    def build(context = nil, *options)
+    # Kinda dirty
+    def base.build(context = nil, *options)
       instance = new(*options)
       instance.instance_variable_set('@_context', context)
       instance
     end
   end
+
+  def self.define_helpers(*dependencies, **renamed_dependencies)
+    dependencies.each do |name|
+      define_method(name) do
+        @_context[name]
+      end
+    end
+
+    renamed_dependencies.each do |original_name, new_name|
+      define_method(new_name) do
+        @_context[original_name]
+      end
+    end
+  end
 end
 
+# InjectContext::Injection.define_helpers should not work on original module
 InjectContext::Injection.freeze
